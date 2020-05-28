07541fdf6e5e0e924b3c4205cae0e70f8e36f8d5
Roopak TJ <mailstorpk@gmail.com>
2020-05-28 13:47:05 +1000

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors());
 const billsRoute = require("./routes/bills"); // This would pull the routes from the posts.js onto app.js and store it in a constant
 //Middleware
 app.use("/.netlify/functions/bills", billsRoute); //This would tell app.js that whenever the API call for posts would happen, then call the constant that save the other file
-exports.handler = serverless(router);
+
 //Route
 
 app.get("/", (req, res) => {
@@ -28,4 +28,5 @@ mongoose.connect(
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => console.log("Connected to DB!")
 );
+module.exports.handler = serverless(app);
 app.listen(8000);
